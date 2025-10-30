3a6fa15ed60915e5a05018017d946d9f9655fab7
bragaxs <160557004+bragaxs@users.noreply.github.com>
2025-10-29 21:53:30 -0400

    criação header e footer

---
+++ front-end/src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Feito com carinho para você. Volte sempre!!
+    </footer>
+  );
+}
+
+export default Footer;

53f0abba4c3cd9f890e72403efd1604e9ec66498
Vilson Fabricio <160530885+Vilson-Fabricio@users.noreply.github.com>
2025-11-27 10:46:58 -0300

    Amigos do Rubens
    a

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,23 +3,38 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+
 import VerMesa from './Pages/Mesas/VerMesa';
 import AtualizarMesa from './Pages/Mesas/AtualizarMesa';
 import CadastrarMesa from './Pages/Mesas/CadastrarMesa';
 import ReservarMesa from './Pages/Mesas/ReservarMesa';
 
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Cadastro from "./Pages/Cadastro";
+import Cardapio from "./Pages/Cardapio";
+import Reservas from "./Pages/Reservas";
+import MinhasReservas from "./Pages/MinhasReservas";
 
 function App() {
   return (
     <Router>
-      <Header /> 
+      <Header />
+
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cardapio" element={<Cardapio />} />
+        <Route path="/reservas" element={<Reservas />} />
+        <Route path="/minhas-reservas" element={<MinhasReservas />} />
         <Route path="/ver-mesas" element={<VerMesa />} />
         <Route path="/atualizar-mesa" element={<AtualizarMesa />} />
         <Route path="/cadastrar-mesa" element={<CadastrarMesa />} />
         <Route path="/reservar-mesa" element={<ReservarMesa />} />
       </Routes>
-      <Footer /> 
+
+      <Footer />
     </Router>
   );
 }
--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -1,21 +1,22 @@
-// src/Router.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginUsuario from "./Pages/Usuarios/loginUsuario";
-import CadastroUsuario from "./Pages/Usuarios/cadastroUsuario";
-import Cardapio from "./Pages/Cardapio/CardapioA";
-import Reservas from "./Pages/Reservas/Reservas";
-import MinhasReservas from "./Pages/Reservas/MinhasReservas";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Cardapio from "./pages/Cardapio";
+import Reservas from "./pages/Reservas";
+import MinhasReservas from "./pages/MinhasReservas";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginUsuario />} />
-        <Route path="/cadastro" element={<CadastroUsuario />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/cardapio" element={<Cardapio />} />
-        <Route path="/reservar" element={<Reservas />} />
+        <Route path="/reservas" element={<Reservas />} />
         <Route path="/minhas-reservas" element={<MinhasReservas />} />
-        <Route path="*" element={<LoginUsuario />} />
       </Routes>
     </BrowserRouter>
   );

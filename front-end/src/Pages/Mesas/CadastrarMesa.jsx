76614f2a1dd9a8cb2718cb6a6db8ab8e451cff6f
bragaxs <vitorioaparecido867@gmail.com>
2025-11-27 22:15:52 -0400

    test - error 404

---
+++ front-end/src/Pages/Mesas/CadastrarMesa.jsx
@@ -0,0 +1,47 @@
+import '../../App.css';
+import Header from '.../../Components/Header';
+import Footer from '.../../Components/Footer';
+import { Link } from 'react-router-dom';
+import '../Mesas/global.css';
+
+function CadastrarMesa() {
+  return (
+    <>
+  
+      <div className="cadastrar-wrapper">
+        <div className="cadastrar-container">
+          <h2 className="titulo">Cadastrar Mesa</h2>
+
+          <form>
+            <div className="input-card">
+              <h4>Número da Mesa</h4>
+              <input type="text" placeholder="Digite o número da mesa" />
+            </div>
+
+            <div className="input-card">
+              <h4>Capacidade da Mesa</h4>
+              <input type="number" placeholder="Digite a capacidade da mesa" />
+            </div>
+
+            <div className="input-card">
+              <h4>Status da Mesa</h4>
+              <select>
+                <option value="ativo">Ativo</option>
+                <option value="inativo">Inativo</option>
+              </select>
+            </div>
+
+            <div className="btn-group">
+              <button type="submit" className="btn btn-primary">Salvar</button>
+              <button type="reset" className="btn btn-warning">Editar / Excluir</button>
+              <Link to="/mesas" className="btn btn-secondary">Cancelar</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+   
+    </>
+  );
+}
+
+export default CadastrarMesa;
